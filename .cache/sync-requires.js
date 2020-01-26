16c7a16f4b91a50e61fce6f2c274ff8172338f3d
Zachary R Newton <zacharyrnewton@gmail.com>
2020-01-26 18:59:51 -0500

    added fonts folder with fonts

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,8 +5,8 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/zacharyrnewton/Desktop/studiogurr/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/zacharyrnewton/Desktop/studiogurr/src/pages/index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/zacharyrnewton/Desktop/studiogurr/src/pages/page-2.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\zacha\\Desktop\\studiogurr\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\zacha\\Desktop\\studiogurr\\src\\pages\\404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\zacha\\Desktop\\studiogurr\\src\\pages\\index.js")))
 }
 
