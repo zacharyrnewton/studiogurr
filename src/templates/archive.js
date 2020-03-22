80d0ad4dcff70b9dbd9c19672c2c6f2366605d08
Zachary Newton <zacharyrnewton@gmail.com>
2020-03-21 22:21:55 -0400

    optimized gallery images

--- src/templates/archive.js
+++ src/templates/archive.js
@@ -4,17 +4,18 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { RichText, Date } from 'prismic-reactjs'
 import style from "../sass/modules/archive.module.sass"
+import Img from "gatsby-image"
 
 const Archive = ({ data }) => {
   return (
     <Layout>
       <SEO title={RichText.asText(data.prismic.archive.title)} />
       <div className={style.archiveContentWrapper}>
-        <img src={data.prismic.archive.hero_image.url} alt={data.prismic.archive.hero_image.alt}/>
+        <Img fluid={data.prismic.archive.hero_imageSharp.childImageSharp.fluid} alt={data.prismic.archive.hero_image.alt}/>
         <div className={style.bodyContentWrapper}>
           <div className={style.bodyContent}>
             <div className={style.aside}>
-  <p>◊ <br />Archived {Date(data.prismic.archive.year).getFullYear()}</p>
+              <p>◊ <br />Archived {Date(data.prismic.archive.year).getFullYear()}</p>
               {RichText.render(data.prismic.archive.services)}
             </div>
             <div className={style.body}>
@@ -31,13 +32,13 @@ const Archive = ({ data }) => {
               {RichText.render(data.prismic.archive.production)}
             </div>
           </div>
-          <img src={data.prismic.archive.side_image.url} alt={data.prismic.archive.side_image.alt}/>
+          <Img fluid={data.prismic.archive.side_imageSharp.childImageSharp.fluid} alt={data.prismic.archive.side_image.alt}/>
         </div>
       </div>
       <div className={style.gallery}>
         {data.prismic.archive.gallery.map(image => {
           return (
-              <img src={image.gallery_image.url} alt={image.gallery_image.alt} key={image.gallery_image.url}/>
+            <Img className={style.galleryImage} fluid={image.gallery_imageSharp.childImageSharp.fluid} alt={image.gallery_image.alt} key={image.gallery_imageSharp.id} />
           )
         })}
       </div>
@@ -55,13 +56,40 @@ export const pageQuery = graphql`
         year
         title
         side_image
+        side_imageSharp {
+          id
+          childImageSharp {
+            id
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
         services
         location
         hero_image
+        hero_imageSharp {
+          id
+          childImageSharp {
+            id
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
         collaborators
         body
         gallery {
           gallery_image
+          gallery_imageSharp {
+            id
+            childImageSharp {
+              id
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
         }
       }
     }
