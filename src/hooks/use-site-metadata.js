47e873f6c9859115a7622b6d12c926300efb6b03
Zachary Newton <zacharyrnewton@gmail.com>
2020-04-13 11:09:52 -0400

    Temporary SEO fix

--- src/hooks/use-site-metadata.js
+++ src/hooks/use-site-metadata.js
@@ -1,19 +1,19 @@
-// import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
-// export const useSiteMetadata = () => {
-//   const { site } = useStaticQuery(
-//     graphql`
-//       query SiteMetaData {
-//         site {
-//           siteMetadata {
-//             title
-//             siteUrl
-//             description
-//             author
-//           }
-//         }
-//       }
-//     `
-//   )
-//   return site.siteMetadata
-// }
+export const useSiteMetadata = () => {
+  const { site } = useStaticQuery(
+    graphql`
+      query SiteMetaData {
+        site {
+          siteMetadata {
+            title
+            siteUrl
+            description
+            author
+          }
+        }
+      }
+    `
+  )
+  return site.siteMetadata
+}
