e7c9417b57f33a5169bcca63f64bbd636c470b72
Zachary Newton <zacharyrnewton@gmail.com>
2020-04-14 15:47:51 -0400

    Updated contact phone number

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,6 +1,6 @@
 module.exports = [{
       plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"Studio Gurr","short_name":"Studio Gurr","start_url":"/","background_color":"#000000","theme_color":"#0000FF","display":"minimal-ui","icon":"src/images/favicon.png"},
+      options: {"plugins":[],"name":"Studio Gurr","short_name":"Studio Gurr","start_url":"/","background_color":"#000000","theme_color":"#000000","display":"minimal-ui","icon":"src/images/favicon.png"},
     },{
       plugin: require('../node_modules/gatsby-source-prismic-graphql/gatsby-browser.js'),
       options: {"plugins":[],"repositoryName":"studiogurr","path":"/preview","previews":true},
