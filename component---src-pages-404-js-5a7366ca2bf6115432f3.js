31388839b22b8db2f1a385080f3782daf9460a9d
Zachary R Newton <zacharyrnewton@gmail.com>
2021-09-06 00:16:07 -0400

    Updates

---
+++ component---src-pages-404-js-5a7366ca2bf6115432f3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstudiogurr=self.webpackChunkstudiogurr||[]).push([[883],{4627:function(e,t,n){n.r(t),n.d(t,{default:function(){return s}});var u=n(7294),r=n(7269),l=n(8453),s=function(){return u.createElement(r.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("h1",{className:"_404-module--errorTitle--2bOXT"},"404, Page not found"),u.createElement("p",null,"You just hit a URL that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-5a7366ca2bf6115432f3.js.map
